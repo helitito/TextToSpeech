6b4797c150d72b456f7f4db792d478af713bbd1b
Hélio Gonçalves <163847245+helitito@users.noreply.github.com>
2024-11-04 16:19:26 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\helio\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\helio\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech"
+  "FLUTTER_ROOT=C:\\Users\\helio\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech"
+  "FLUTTER_TARGET=C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\helio\\OneDrive\\TextToSpeech\\text_to_speech\\.dart_tool\\package_config.json"
+)
